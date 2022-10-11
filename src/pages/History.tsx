3608e8f4fc579ce27a43ad0b8ada85d55676ff69
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-10-11 16:49:14 +0530

    Update History.tsx

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -41,7 +41,7 @@ const History: FunctionComponent<HistoryProps> = () => {
 
   return (
     <>
-      <Title value="History | Moonlight" />
+      <Title value="History | GoGoMoviw" />
       <FilmListViewForBookmarkAndHistory
         films={recentlyWatchFilms}
         isLoading={isLoading}
