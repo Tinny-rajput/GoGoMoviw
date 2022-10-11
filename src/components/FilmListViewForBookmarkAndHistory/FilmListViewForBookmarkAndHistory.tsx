af9c7e993b92938c9766b87a6e8bbc2be0b3ce2c
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-10-11 17:11:42 +0530

    Update FilmListViewForBookmarkAndHistory.tsx

--- src/components/FilmListViewForBookmarkAndHistory/FilmListViewForBookmarkAndHistory.tsx
+++ src/components/FilmListViewForBookmarkAndHistory/FilmListViewForBookmarkAndHistory.tsx
@@ -130,7 +130,7 @@ const FilmListViewForBookmarkAndHistory: FunctionComponent<
             className="h-10 w-10 rounded-full object-cover"
           />
           <p className="text-xl text-white font-medium tracking-wider uppercase">
-            Moon<span className="text-primary">light</span>
+            GoGo<span className="text-primary">Moviw</span>
           </p>
         </Link>
         <button onClick={() => setIsSidebarActive((prev) => !prev)}>
