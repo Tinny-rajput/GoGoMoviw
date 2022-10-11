24db590ab1b77cb727c1eae06a0e746798e56feb
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-10-11 17:20:14 +0530

    Update ModalNotification.tsx

--- src/components/Auth/ModalNotification.tsx
+++ src/components/Auth/ModalNotification.tsx
@@ -58,7 +58,7 @@ const ModalNotification: FunctionComponent<ModalNotificationProps> = ({
           {message}
           <br></br>
           {type === "error" && <span>Keep calm and try again.</span>}
-          {type === "success" && <span>Let's "Moonlight and chill".</span>}
+          {type === "success" && <span>Let's "Netflix and chill".</span>}
         </p>
         <button
           onClick={() => {
