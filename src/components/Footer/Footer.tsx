872e33d7881567d07cd4a8f94a04a367715e7900
Tin <72661783+Tinny-rajput@users.noreply.github.com>
2022-10-11 17:06:20 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import { AiFillGithub } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
 
 interface FooterProps {}
 
@@ -8,14 +8,14 @@ const Footer: FunctionComponent<FooterProps> = () => {
   return (
     <div className="bg-dark-lighten text-white flex justify-between items-center py-3 px-4 shadow-md mt-3">
       <p className="flex gap-2">
-        <span>Copyright _Fuocy</span>
-        <span className="hidden md:block"> &copy; 19/7/2022</span>
+        <span>Copyright GoGoMoviw</span>
+        <span className="hidden md:block"> &copy; 2022</span>
       </p>
       <div className="flex gap-3 items-center">
         <p className="hidden md:block">Contact me: </p>
         <div className="flex gap-2">
           <a
-            href="https://github.com/fuocy"
+          href="https://github.com/Tinny-rajput"
             target="_blank"
             rel="noreferrer"
             className="hover:text-[#6e5494] transition duration-300"
@@ -23,12 +23,12 @@ const Footer: FunctionComponent<FooterProps> = () => {
             <AiFillGithub size={25} />
           </a>
           <a
-            href="https://www.facebook.com/fuocy"
+            href="https://www.instagram.com/tinny_rajput8"
             target="_blank"
             rel="noreferrer"
             className="hover:text-primary transition duration-300"
           >
-            <BsFacebook size={22} />
+            <BsInstagram size={22} />
           </a>
         </div>
       </div>
